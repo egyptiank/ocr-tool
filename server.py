a62c281dc62a0e89fa9f1f4bae732949cebf4d12
egyptiank <91704088+egyptiank@users.noreply.github.com>
2021-10-01 19:46:24 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI, Request, File, UploadFile, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 import shutil
-import ocr
 import os
-import uuid
-import json
+import requests
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -16,28 +14,9 @@ def home(request: Request):
 @app.post("/api/v1/extract_text")
 async def extract_text(image: UploadFile = File(...)):
     temp_file = _save_file_to_disk(image, path="temp", save_as="temp")
-    text = await ocr.read_image(temp_file)
+    text = await colorize(temp_file)
     return {"filename": image.filename, "text": text}
 
-@app.post("/api/v1/bulk_extract_text")
-async def bulk_extract_text(request: Request, bg_task: BackgroundTasks):
-    images = await request.form()
-    folder_name = str(uuid.uuid4())
-    os.mkdir(folder_name)
-
-    for image in images.values():
-        temp_file = _save_file_to_disk(image, path=folder_name, save_as=image.filename)
-
-    bg_task.add_task(ocr.read_images_from_dir, folder_name, write_to_file=True)
-    return {"task_id": folder_name, "num_files": len(images)}
-
-@app.get("/api/v1/bulk_output/{task_id}")
-async def bulk_output(task_id):
-    text_map = {}
-    for file_ in os.listdir(task_id):
-        if file_.endswith("txt"):
-            text_map[file_] = open(os.path.join(task_id, file_)).read()
-    return {"task_id": task_id, "output": text_map}
 
 def _save_file_to_disk(uploaded_file, path=".", save_as="default"):
     extension = os.path.splitext(uploaded_file.filename)[-1]
@@ -45,3 +24,16 @@ def _save_file_to_disk(uploaded_file, path=".", save_as="default"):
     with open(temp_file, "wb") as buffer:
         shutil.copyfileobj(uploaded_file.file, buffer)
     return temp_file
+
+
+async def colorize(path):
+    r = requests.post(
+        "https://api.deepai.org/api/colorizer",
+        files={
+            'image': open(path, 'rb'),
+        },
+        headers={'api-key': '3a1506b8-06bd-44a2-9bdf-305964d21330'}
+    )
+
+    res = 'https:'+str(r.json()).split(':')[-1]
+    return res[:len(res)-2]
