622e95a9478635819516fbd0d92139fe40ae9224
egyptiank <91704088+egyptiank@users.noreply.github.com>
2021-10-02 16:38:07 +0200

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,49 +1,110 @@
-import pytesseract
+'''
+@paper: GAN Prior Embedded Network for Blind Face Restoration in the Wild (CVPR2021)
+@author: yangxy (yangtao9009@gmail.com)
+'''
 import os
-import sys
+import cv2
+import glob
+import time
+import numpy as np
+from PIL import Image
+import __init_paths
+from retinaface.retinaface_detection import RetinaFaceDetection
+from face_model.face_gan import FaceGAN
+from align_faces import warp_and_crop_face, get_reference_facial_points
+from skimage import transform as tf
 
+class FaceEnhancement(object):
+    def __init__(self, base_dir='./', size=512, model=None, channel_multiplier=2, narrow=1):
+        self.facedetector = RetinaFaceDetection(base_dir)
+        self.facegan = FaceGAN(base_dir, size, model, channel_multiplier, narrow)
+        self.size = size
+        self.threshold = 0.9
 
-async def read_image(img_path, lang='eng'):
-    """
-    Performs OCR on a single image
+        # the mask for pasting restored faces back
+        self.mask = np.zeros((512, 512), np.float32)
+        cv2.rectangle(self.mask, (26, 26), (486, 486), (1, 1, 1), -1, cv2.LINE_AA)
+        self.mask = cv2.GaussianBlur(self.mask, (101, 101), 11)
+        self.mask = cv2.GaussianBlur(self.mask, (101, 101), 11)
 
-    :img_path: str, path to the image file
-    :lang: str, language to be used while conversion (optional, default is english)
+        self.kernel = np.array((
+                [0.0625, 0.125, 0.0625],
+                [0.125, 0.25, 0.125],
+                [0.0625, 0.125, 0.0625]), dtype="float32")
 
-    Returns
-    :text: str, converted text from image
-    """
+        # get the reference 5 landmarks position in the crop settings
+        default_square = True
+        inner_padding_factor = 0.25
+        outer_padding = (0, 0)
+        self.reference_5pts = get_reference_facial_points(
+                (self.size, self.size), inner_padding_factor, outer_padding, default_square)
 
-    try:
-        return pytesseract.image_to_string(img_path, lang=lang)
-    except:
-        return "[ERROR] Unable to process file: {0}".format(img_path)
+    def process(self, img):
+        facebs, landms = self.facedetector.detect(img)
+        
+        orig_faces, enhanced_faces = [], []
+        height, width = img.shape[:2]
+        full_mask = np.zeros((height, width), dtype=np.float32)
+        full_img = np.zeros(img.shape, dtype=np.uint8)
 
-async def read_images_from_dir(dir_path, lang='eng', write_to_file=False):
-    """
-    Performs OCR on all images present in a directory
+        for i, (faceb, facial5points) in enumerate(zip(facebs, landms)):
+            if faceb[4]<self.threshold: continue
+            fh, fw = (faceb[3]-faceb[1]), (faceb[2]-faceb[0])
 
-    :dir_path: str, path to the directory of images
-    :lang: str, language to be used while conversion (optional, default is english)
+            facial5points = np.reshape(facial5points, (2, 5))
 
-    Returns
-    :converted_text: dict, mapping of filename to converted text for each image
-    """
+            of, tfm_inv = warp_and_crop_face(img, facial5points, reference_pts=self.reference_5pts, crop_size=(self.size, self.size))
+            
+            # enhance the face
+            ef = self.facegan.process(of)
+            
+            orig_faces.append(of)
+            enhanced_faces.append(ef)
+            
+            tmp_mask = self.mask
+            tmp_mask = cv2.resize(tmp_mask, ef.shape[:2])
+            tmp_mask = cv2.warpAffine(tmp_mask, tfm_inv, (width, height), flags=3)
 
-    converted_text = {}
-    for file_ in os.listdir(dir_path):
-        if file_.endswith(('png', 'jpeg', 'jpg')):
-            text = await read_image(os.path.join(dir_path, file_), lang=lang)
-            converted_text[os.path.join(dir_path, file_)] = text
-    if write_to_file:
-        for file_path, text in converted_text.items():
-            _write_to_file(text, os.path.splitext(file_path)[0] + ".txt")
-    return converted_text
+            if min(fh, fw)<100: # gaussian filter for small faces
+                ef = cv2.filter2D(ef, -1, self.kernel)
+            
+            tmp_img = cv2.warpAffine(ef, tfm_inv, (width, height), flags=3)
 
-def _write_to_file(text, file_path):
-    """
-    Helper method to write text to a file
-    """
-    print("[INFO] Writing text to file: {0}".format(file_path))
-    with open(file_path, 'w') as fp:
-        fp.write(text)
+            mask = tmp_mask - full_mask
+            full_mask[np.where(mask>0)] = tmp_mask[np.where(mask>0)]
+            full_img[np.where(mask>0)] = tmp_img[np.where(mask>0)]
+
+        full_mask = full_mask[:, :, np.newaxis]
+        img = cv2.convertScaleAbs(img*(1-full_mask) + full_img*full_mask)
+
+        return img, orig_faces, enhanced_faces
+        
+
+if __name__=='__main__':
+    #model = {'name':'GPEN-BFR-512', 'size':512, 'channel_multiplier':2, 'narrow':1}
+    model = {'name':'GPEN-BFR-256', 'size':256, 'channel_multiplier':1, 'narrow':0.5}
+    
+    indir = 'examples/imgs'
+    outdir = 'examples/outs-BFR'
+    os.makedirs(outdir, exist_ok=True)
+
+    faceenhancer = FaceEnhancement(size=model['size'], model=model['name'], channel_multiplier=model['channel_multiplier'], narrow=model['narrow'])
+
+    files = sorted(glob.glob(os.path.join(indir, '*.*g')))
+    for n, file in enumerate(files[:]):
+        filename = os.path.basename(file)
+        
+        im = cv2.imread(file, cv2.IMREAD_COLOR) # BGR
+        if not isinstance(im, np.ndarray): print(filename, 'error'); continue
+        im = cv2.resize(im, (0,0), fx=2, fy=2)
+
+        img, orig_faces, enhanced_faces = faceenhancer.process(im)
+        
+        cv2.imwrite(os.path.join(outdir, '.'.join(filename.split('.')[:-1])+'_COMP.jpg'), np.hstack((im, img)))
+        cv2.imwrite(os.path.join(outdir, '.'.join(filename.split('.')[:-1])+'_GPEN.jpg'), img)
+        
+        for m, (ef, of) in enumerate(zip(enhanced_faces, orig_faces)):
+            of = cv2.resize(of, ef.shape[:2])
+            cv2.imwrite(os.path.join(outdir, '.'.join(filename.split('.')[:-1])+'_face%02d'%m+'.jpg'), np.hstack((of, ef)))
+        
+        if n%10==0: print(n, filename)
